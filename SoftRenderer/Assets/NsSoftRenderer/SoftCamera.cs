50f68bbb9f8bb0ed6f36de1571250b4d013548b9
billwillman <billwillman@qq.com>
2020-04-17 22:18:04 +0800

    1

--- SoftRenderer/Assets/NsSoftRenderer/SoftCamera.cs
+++ SoftRenderer/Assets/NsSoftRenderer/SoftCamera.cs
@@ -17,25 +17,55 @@ namespace NsSoftRenderer {
         private Vector3 m_LookAt = new Vector3(0, 0, -1f);
         // UP方向
         private Vector3 m_Up = new Vector3(0, 1f, 0);
+        // Right方向
         private Vector3 m_Right = Vector3.zero;
+        // 位置
+        private Vector3 m_Position = Vector3.zero;
         private bool m_IsLookAtAndUpChged = true;
+        private bool m_IsMustChgMatrix = true;
+        // 观测和投影矩阵
+        private Matrix4x4 m_ViewProjMatrix = Matrix4x4.identity;
 
-        private void UpdateRightAxis() {
+        // 更新轴
+        private void UpdateAxis() {
             if (m_IsLookAtAndUpChged) {
                 m_IsLookAtAndUpChged = false;
-                m_Right = Vector3.Cross(m_LookAt, m_Up);
+                m_LookAt = m_LookAt.normalized;
+                m_Right = Vector3.Cross(m_LookAt, m_Up).normalized;
+                m_Up = Vector3.Cross(m_Right, m_LookAt);
+            }
+        }
+
+        private void UpdateMatrix() {
+            if (m_IsMustChgMatrix) {
+                m_IsMustChgMatrix = false;
+                // 更新矩阵
             }
         }
 
         public Vector3 Right {
             get {
-                UpdateRightAxis();
+                UpdateAxis();
                 return m_Right;
             }
         }
 
+        public Vector3 Position {
+            get {
+                return m_Position;
+            }
+
+            set {
+                if (m_Position != value) {
+                    m_Position = value;
+                    m_IsMustChgMatrix = true;
+                }
+            }
+        }
+
         public void Update(float delta) {
-            UpdateRightAxis();
+            UpdateAxis();
+            UpdateMatrix();
         }
 
         // 摄影机类型
