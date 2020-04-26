fe89fdf0b5e42b8a2e251965db45b9dc556f87ed
billwillman <billwillman@qq.com>
2020-04-26 14:53:55 +0800

    3

--- SoftRenderer/Assets/NsSoftRenderer/Core/SoftMath.cs
+++ SoftRenderer/Assets/NsSoftRenderer/Core/SoftMath.cs
@@ -148,6 +148,8 @@ namespace NsSoftRenderer {
          *   拆分得到：
          *   => [u, v, 1] * [ABx, ACx, PAx] = 0
          *      [u, v, 1] * [ABy, ACy, PAy] = 0
+         *      [u, v, 1] * [ABz, ACz, PAz] = 0
+         *    两个式子足够了，所以只取前两个用于计算。
          *   【结论】也就是求同时垂直 [ABx, ACx, PAx]和[ABy, ACy, PAy]的向量，也就是这两个向量的叉乘。
          *   【注意】矩阵变换后的三角形的重心和原来三角形的重心可能会不一致。。。
         */
--- SoftRenderer/Assets/NsSoftRenderer/Test/SoftMathTest.cs
+++ SoftRenderer/Assets/NsSoftRenderer/Test/SoftMathTest.cs
@@ -19,5 +19,11 @@ public class SoftMathTest : MonoBehaviour
         float u1, v1, r1;
         SoftMath.GetBarycentricCoordinate(ref A, ref B, ref C, ref P, out u1, out v1, out r1);
         Debug.LogFormat("【origin】u: {0} v: {1} r: {2}【SoftMth】u: {3} v: {4} r: {5}", u, v, r, u1, v1, r1);
+
+        Vector3 AB = B - A;
+        Vector3 AC = C - A;
+        Vector3 PA = A - P;
+        Vector3 v3 = new Vector3(AB.z, AC.z, PA.z);
+        Debug.LogFormat("v3: {0}", v3.x * u1, v3.y * v1, v3.z * r1);
     }
 }
