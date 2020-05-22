e6961a59946c51a056f02c1f1aea024d8e1e4bbd
billwillman <billwillman@qq.com>
2020-05-22 20:48:02 +0800

    2

--- SoftRenderer/Assets/NsSoftRenderer/Core/SoftCamera.cs
+++ SoftRenderer/Assets/NsSoftRenderer/Core/SoftCamera.cs
@@ -124,7 +124,7 @@ namespace NsSoftRenderer {
                  * 推到过程：
                  * 1）普通点：视锥体【任意点】(x0, y0, z0) 到最终变换到近平面点(x1, y1, z1(Unknown))，根据相似三角形推出
                  *    y1 = near/z0 * y0;    x1 = near/z0 * x0;  Z1为UNKOWN，因为是一个矩形盒子里。
-                 *    最终变换坐标为：(near/z0 * x0, near/z0 * y0, z0), 根据齐次坐标定义，都乘以Z得到齐次坐标：(near * x0, near * y0, z1(unknow(?)), z0),这样仍然描述同一个点。
+                 *    最终变换坐标为：(near/z0 * x0, near/z0 * y0, z1), 根据齐次坐标定义，都乘以Z得到齐次坐标：(near * x0, near * y0, z1(unknow(?)), z0),这样仍然描述同一个点。
                  *    这样构造出一个矩阵结论：
                  *    near, 0, 0, 0
                  *    0, near, 0, 0
